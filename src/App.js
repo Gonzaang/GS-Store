368e1dc6e11b7df7c72468053a2390c2950cae99
gonza garnica <gonza.garnica@hotmail.com>
2020-11-02 20:39:23 -0300

    Items prueba 2

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ function App() {
   return ( <>
     <NavBar />
     <ItemListContainer titulo="Encontrá aquí los mejores precios" />
-    <ItemList items= {[1]} />
+    <Item id="1" title="Ryzen 5" price="$17000" pictureUrl="../Images/cpu.jpg"  />
+    <Item id="2" title="Ram 8GB" price="$5000" pictureUrl="../Images/ram.jpg"  />
+    <Item id="3" title="Rtx 3080" price="$99000" pictureUrl="../Images/gpu.jpg"  />    
     
     
     </>
--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -2,6 +2,18 @@ import React from 'react';
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 
-function Item ({Item}) {
-  
-}
+export default function Item (props) {
+  return ( 
+  <>
+    <Card style={{ width: '12rem' }}>
+    <Card.Img variant="top" src={props.pictureUrl}/>
+    <Card.Body>
+    <Card.Title>{props.title}</Card.Title>
+    <Card.Text>{props.price}</Card.Text>
+    <Button variant="primary">Agregar</Button>
+    </Card.Body>
+    </Card>
+   </>
+  )
+};
+    
